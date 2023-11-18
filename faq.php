c9f71e303ee1a70c237de3aa1d86d464e7f6e5d9
SVL Automations <info@svlautomations.in>
2023-11-18 22:30:20 +0530

    Added code for website and email

---
+++ faq.php
@@ -0,0 +1,130 @@
+<!-- ======= F.A.Q Section ======= -->
+<section id="faq" class="faq">
+
+  <div class="container" data-aos="fade-up">
+
+    <header class="section-header">
+      <h2>F.A.Q</h2>
+      <p>Frequently Asked Questions</p>
+    </header>
+
+    <div class="row">
+      <div class="col-lg-6">
+        <!-- F.A.Q List 1-->
+        <div class="accordion accordion-flush" id="faqlist1">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                What are the main servies we serve?
+              </button>
+            </h2>
+            <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+              <div class="accordion-body">
+                <p> We are providing different services </p>
+                <p><i class="ri-wifi-fill"></i> Internet of Things</p>
+                <p><i class="ri-html5-fill"></i> Web Development</p>
+                <p><i class="ri-user-voice-fill"></i> Academic Training</p>
+                <p><i class="ri-upload-cloud-fill"></i> Industrial Automation</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                What are software outsourcing services?
+              </button>
+            </h2>
+            <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+              <div class="accordion-body">
+                <p>
+                  <b>Software outsourcing</b> is a remote service that is provided by software development companies. The service can consist of services ranging from full software or mobile application development, design development, testing, to project launch and project support.
+                </p>
+                <p>
+                  So, in the simplest words, software outsourcing services is <b>all about hiring a developer or a development team that is external to your company staff to help create some digital product for your business.</b>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                How long does it take to start my product development?
+              </button>
+            </h2>
+            <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+              <div class="accordion-body">
+                <p>
+                  To start with, we <b>review project requirements from the client</b>, we get them analyzed and then <b> you of the estimation results</b>.
+                </p>
+                <p>
+                  Once you are fine with the cost and time, <b>the next step would be to create a project report document and a proposal</b>. Our business analyst will prepare this and then we send you the same for review. Once everything has been finalized we would then take the project to the next stage, which is the development stage.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+
+        <!-- F.A.Q List 2-->
+        <div class="accordion accordion-flush" id="faqlist2">
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-1">
+                How do we assure privacy and confidentiality?
+              </button>
+            </h2>
+            <div id="faq2-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist2">
+              <div class="accordion-body">
+                <p>
+                  We sign NDAs and Confidentiality Agreements as required by you. All our employees are full-time employees and are bound by company Confidentiality and Non-Disclosure clauses.
+
+                  Additionally, for sensitive projects, we can sign custom NDAs.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-2">
+                How do you guarantee product quality?
+              </button>
+            </h2>
+            <div id="faq2-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist2">
+              <div class="accordion-body">
+                <p>
+                  We <b>evaluate the result after every two weeks</b>, we test our work (we conduct both development and acceptance tests), we present it to you, we <b>apply your feedback</b> so you know you get what you are paying for.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-3">
+                How can I communicate with SVL Automations?
+              </button>
+            </h2>
+            <div id="faq2-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist2">
+              <div class="accordion-body">
+                <p>
+                  You can reach out to SVL Automations anytime via email, Phone and instant chat.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</section><!-- End F.A.Q Section -->
